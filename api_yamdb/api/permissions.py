bd6dbfdad2fe3b90956e6f0a904390a31fdd1249
tarki2206 <sasha100500@yahoo.com>
2023-08-18 08:19:53 -0700

    5 py errors

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -36,18 +36,6 @@ class IsAuthorOrAdminOrModeratorOrReadOnly(permissions.BasePermission):
 
 class IsAdminModeratorAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        elif request.method == "POST" and request.user.is_authenticated:
-            return True
-        elif request.method == "PATCH":
-            return request.user.is_staff or request.user == obj.author
-        else:
-            return False
-
-
-class IsAdminModeratorAuthorOrReadOnly1(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
             or request.user.is_admin
